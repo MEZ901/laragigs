cfdf3e708a62a63386aa502a7d184c09e251ad4b
issam <issammez44@gmail.com>
2023-01-31 20:53:00 +0100

    fix the migration bug

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,39 +2,10 @@
 
 namespace App\Models;
 
-class Listing {
-    public static function all() {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Listing one',
-                'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat debitis dignissimos enim cum ratione perferendis sapiente id! Quaerat culpa repellat cupiditate quam veritatis facilis repudiandae, aut officia unde, temporibus vero.'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing two',
-                'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat debitis dignissimos enim cum ratione perferendis sapiente id! Quaerat culpa repellat cupiditate quam veritatis facilis repudiandae, aut officia unde, temporibus vero.'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Listing three',
-                'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat debitis dignissimos enim cum ratione perferendis sapiente id! Quaerat culpa repellat cupiditate quam veritatis facilis repudiandae, aut officia unde, temporibus vero.'
-            ],
-            [
-                'id' => 4,
-                'title' => 'Listing four',
-                'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat debitis dignissimos enim cum ratione perferendis sapiente id! Quaerat culpa repellat cupiditate quam veritatis facilis repudiandae, aut officia unde, temporibus vero.'
-            ]
-        ];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id) {
-        $listings = self::all();
-
-        foreach($listings as $listing) {
-            if($id == $listing['id']){
-                return $listing;
-            }
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
