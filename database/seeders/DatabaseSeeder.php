5d02fd96f84cf2bdc565e6bed17e2cfadb7416c6
issam <issammez44@gmail.com>
2023-02-01 10:27:29 +0100

    bring the data from database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,7 +15,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(5)->create();
+
+        Listing::create([
+            'title' => 'Laravel Senior Developer',
+            'tag' => 'laravel, javascript',
+            'company' => 'Acme Crop',
+            'location' => 'Youssoufia, MA',
+            'email' => 'email@email.com',
+            'website' => 'https:://www.acme.com',
+            'description' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium voluptatum natus iusto incidunt. Unde praesentium consectetur, voluptate dolor mollitia minima officia atque, ea doloremque culpa accusamus provident adipisci sint odio.'
+        ]);
+
+        Listing::create([
+            'title' => 'Full-stack Engineer',
+            'tag' => 'laravel, backend, api',
+            'company' => 'Stark Industries',
+            'location' => 'New York, NYC',
+            'email' => 'email2@email.com',
+            'website' => 'https:://www.stackindustries.com',
+            'description' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium voluptatum natus iusto incidunt. Unde praesentium consectetur, voluptate dolor mollitia minima officia atque, ea doloremque culpa accusamus provident adipisci sint odio.'
+        ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
