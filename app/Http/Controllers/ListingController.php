ba79fce1ca7eeb1ed8768f37cab07bacdf1691ea
issam <issammez44@gmail.com>
2023-02-05 16:23:45 +0100

    add flash message after adding a listing

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -37,6 +37,6 @@ class ListingController extends Controller
 
         Listing::create($formFields);
 
-        return redirect('/');
+        return redirect('/')->with('message', 'Listing created successfully!');
     }
 }
